979ba46f7d3272257342910310f83a509b248aba
Mamadou Talla DIA <ibntalla@gmail.com>
2025-05-29 20:10:48 +0100

    feat: update docker-compose and deployment scripts
    - Updated client docker-compose.yml to use dynamic container and image names.
    - Refactored deployment index.ts to include network creation and Dockerfile building functions.
    - Enhanced command execution in deployment scripts to capture stdout and stderr.
    - Added Dockerfile for API service with production setup.
    - Created docker-compose.yml for API service with PostgreSQL database configuration.

--- deploy/index.ts
+++ deploy/index.ts
@@ -42,8 +42,8 @@ interface IEnv {
 
       // await cloneRepo(tempDir, env);
       // await buildEnvFile(tempDir, env);
-
-      await runCmd(`docker network create -d bridge sya-social-${env.code}`);
+      // await createNetwork(env);
+      await buildDockerFile(tempDir, env);
 
       // await run(env);
     } catch (error) {
@@ -96,17 +96,62 @@ interface IEnv {
     writeFileSync(`${tempDir}/client/.env`, clientEnvFileContent);
   }
 
+  async function createNetwork(env: IEnv) {
+    const networkName = `sya-social-${env.code}`;
+    const list = await runCmd(`docker network inspect ${networkName}`);
+    if (!JSON.parse(list).find((net: any) => net.Name === networkName)) {
+      await runCmd(`docker network create -d bridge ${networkName}`);
+    }
+  }
+
+  async function buildDockerFile(tempDir: string, env: IEnv) {
+    let dockerCompose = await Bun.file(
+      join(tempDir, "client/docker-compose.yml")
+    ).text();
+    dockerCompose = dockerCompose
+      .replaceAll("$CONTAINER_NAME", `sya-social-${env.code}-client`)
+      .replaceAll("$NETWORK", `sya-social-${env.code}`);
+
+    writeFileSync(join(tempDir, "client/docker-compose.yml"), dockerCompose);
+  }
+
   async function runCmd(command: string, options: { cwd?: string } = {}) {
+    async function readStream(
+      stream?: ReadableStream<Uint8Array>
+    ): Promise<string> {
+      if (!stream) return "";
+      const reader = stream.getReader();
+      const decoder = new TextDecoder();
+      let result = "";
+
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        result += decoder.decode(value, { stream: true });
+      }
+
+      return result;
+    }
+
     console.log(`▶️ ${command}`);
     const proc = Bun.spawn(["bash", "-c", command], {
-      stdout: "inherit",
-      stderr: "inherit",
+      stdout: "pipe",
+      stderr: "pipe",
       cwd: options.cwd,
     });
+
+    const [stdout, stderr] = await Promise.all([
+      readStream(proc.stdout),
+      readStream(proc.stderr),
+    ]);
+
     const exitCode = await proc.exited;
+
     if (exitCode !== 0) {
       console.error(`❌ Erreur lors de l'exécution : ${command}`);
       process.exit(exitCode);
     }
+
+    return stdout.trim();
   }
 })();
